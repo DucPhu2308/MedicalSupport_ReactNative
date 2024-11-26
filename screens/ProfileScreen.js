66277e67dfa5a9441c4be303fb32722e6add7d34
TrongPhucNguyen <trongnguyenphuc22082003@gmail.com>
2024-11-26 23:52:33 +0700

    update

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -32,7 +32,8 @@ const ProfileScreen = ({ route }) => {
     const fetchPosts = async (id) => {
         try {
             const res = await PostAPI.getPostByUserId(id);
-            const sortedPosts = res.data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+            const filteredPosts = res.data.filter(post => post.status === 'PUBLISHED');
+            const sortedPosts = filteredPosts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
             setPosts(sortedPosts);
         } catch (error) {
             console.error(error);
